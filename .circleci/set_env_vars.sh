770751a7870d5dd19a6e0a14d47aacf42f4a0fbb
Jon Gaulding <jon@itso.io>
2019-08-10 17:10:05 -0700

    Add CircleCI config

---
+++ .circleci/set_env_vars.sh
@@ -0,0 +1,4 @@
+echo 'export PYTHONPATH=${HOME}/google_appengine:$PYTHONPATH' >> $BASH_ENV
+echo 'export SERVER_CODE_ROOT=server' >> $BASH_ENV
+echo 'export SERVER_SRC_ROOT=server/src' >> $BASH_ENV
+echo 'export FRONTEND_ROOT=frontend' >> $BASH_ENV
