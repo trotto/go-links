3dbc6860a457515b34bc8ef9f846131928ca22a5
jon-labs <jon@itso.io>
2020-04-04 14:34:22 -0500

    Remove the need to build the frontend anew when simply deploying a new instance

--- server/deploy.sh
+++ server/deploy.sh
@@ -6,10 +6,6 @@ python prepare_deploy.py
 
 sh install_deps.sh
 
-cd ../frontend
-sh build_bundle.sh
-cd ../server
-
 source src/config/.set_env_vars
 
 gcloud app deploy --project $TROTTO_APP_ID -q -v $(date +%s) src/app.yaml src/index.yaml src/queue.yaml
