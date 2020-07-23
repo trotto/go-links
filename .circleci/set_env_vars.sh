510aa2e0a31a88a46b356a34e9860972151e6baf
itsojon <jon@itso.io>
2020-07-23 18:44:53 -0500

    Finish

--- .circleci/set_env_vars.sh
+++ .circleci/set_env_vars.sh
@@ -4,4 +4,4 @@ echo 'export SERVER_SRC_ROOT=server/src' >> $BASH_ENV
 echo 'export FRONTEND_ROOT=frontend' >> $BASH_ENV
 version=$(date +%s)
 echo "export NEW_TROTTO_VERSION_ID=${version}" >> $BASH_ENV
-echo "TEST_INSTANCE_BASE=https://${version}-dot-${GCLOUD_PROJECT}.appspot.com" >> $BASH_ENV
+echo "export TEST_INSTANCE_BASE=https://${version}-dot-${GCLOUD_PROJECT}.appspot.com" >> $BASH_ENV
