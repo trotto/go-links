f9b52a7b5942dfbd23a8632018e661d85235bd08
itsojon <jon@itso.io>
2021-02-20 12:16:00 -0600

    Fix org settings request and delete dead code

--- server/src/shared_helpers/env.py
+++ server/src/shared_helpers/env.py
@@ -1,19 +1,5 @@
 import os
 
 
-APP_ENGINE_PLATFORM_ID = 'app_engine'
-
-
-def get_platform():
-  return os.getenv('PLATFORM', APP_ENGINE_PLATFORM_ID)
-
-
-def current_env_is_production():
-  if get_platform() == APP_ENGINE_PLATFORM_ID:
-    return os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/')
-
-  return os.getenv('ENVIRONMENT') == 'prod'
-
-
 def current_env_is_local():
   return os.getenv('ENVIRONMENT') in ['dev', 'test_env']
