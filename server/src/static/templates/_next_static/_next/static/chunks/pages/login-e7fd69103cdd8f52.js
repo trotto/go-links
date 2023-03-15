20814c3386a9a3b5af542f99860714e9664ac4ec
Slava Bihieiev <mephistozp@gmail.com>
2023-03-15 19:21:02 +0200

    New build

---
+++ server/src/static/templates/_next_static/_next/static/chunks/pages/login-e7fd69103cdd8f52.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[459],{3700:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/login",function(){return _(2882)}])},2882:function(n,u,_){"use strict";_.r(u);var t=_(4897);u.default=t.Xs}},function(n){n.O(0,[774,888,179],function(){return n(n.s=3700)}),_N_E=n.O()}]);
