f488222b0ff7c29acf5b6cd937ffcaf509a2c003
mef-bomb <mephistozp@gmail.com>
2023-03-15 19:30:25 +0200

    New build (#204)

---
+++ server/src/static/templates/_next_static/_next/static/chunks/pages/index-8bf964c4c3e847d9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8312:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(4186)}])},4186:function(n,u,_){"use strict";_.r(u);var t=_(4897);u.default=t.bT}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8312)}),_N_E=n.O()}]);
