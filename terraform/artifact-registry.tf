9e33ae857ea2278cfda64a371f7603611508adcc
yaz-saleh <74604867+yaz-saleh@users.noreply.github.com>
2023-11-21 23:42:27 -0600

    Fix: typo in path (#2)
    * fix: typo in path

--- terraform/artifact-registry.tf
+++ terraform/artifact-registry.tf
@@ -8,7 +8,7 @@ resource "google_artifact_registry_repository" "this" {
 resource "null_resource" "image" {
   depends_on = [google_artifact_registry_repository.this]
   provisioner "local-exec" {
-    command = "cd ../.. && ./docker_scripts/image.sh ${var.deploy} ${var.region} ${var.project} ${var.app-name} ${local.version}"
+    command = "cd ${path.module}/.. && ./docker_scripts/image.sh ${var.deploy} ${var.region} ${var.project} ${var.app-name} ${local.version}"
   }
   triggers = {
     always_run = timestamp()
