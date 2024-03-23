8bfed74523f99c8afb2804f7aacce7e70c2da22b
Clinton Blackburn <clinton.blackburn@gmail.com>
2024-03-23 13:13:51 -0700

    Fixed pool_pre_ping configuration
    apply_pool_defaults was removed in flask-sqlalchemy 3.x. `SQLALCHEMY_ENGINE_OPTIONS` is used to enable pool_pre_ping, and avoid 500 errors if the database connection during a pronlonged period between requests.

--- server/src/db.py
+++ server/src/db.py
@@ -1,8 +1,3 @@
-from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
-
-class SQLAlchemy(_BaseSQLAlchemy):
-  def apply_pool_defaults(self, app, options):
-    super(SQLAlchemy, self).apply_pool_defaults(app, options)
-    options["pool_pre_ping"] = True
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
